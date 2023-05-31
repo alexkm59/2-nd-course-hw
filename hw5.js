41c6d34f888afa0bf0757afa7db2b34230c26941
alexkm59 <alexkm59@yandex.ru>
2023-05-31 14:37:43 +0500

    task 6-7

--- hw5.js
+++ hw5.js
@@ -38,29 +38,68 @@ function hello() {
     if (age < 0) {
         alert(`Вы ввели неправильное значение`);
     }
-        else{
-            if (age >= 0 && age <= 12) {
-                alert(`Привет, друг!`);
-                
-            } else {
-                alert(`Добро пожаловать!`);
-            }
-        }  
+    else {
+        if (age >= 0 && age <= 12) {
+            alert(`Привет, друг!`);
+
+        } else {
+            alert(`Добро пожаловать!`);
+        }
+    }
 }
 
 hello();
 
 // Задание 5
 
-function multiplay (a, b) {
-a = Number(a);
-b = Number(b);
-  
-if (isNaN(a) || isNaN(b)) {
-    return 'введенное значение не является числом';
-} else{
-    return a * b;
-}
+function multiplay(a, b) {
+    a = Number(a);
+    b = Number(b);
+
+    if (isNaN(a) || isNaN(b)) {
+        return 'введенное значение не является числом';
+    } else {
+        return a * b;
+    }
 }
 
-console.log(multiplay (40, 4));
+console.log(multiplay(40, 4));
+
+// Задание 6
+
+function exponent() {
+    a = Number(prompt(`Введите число`));
+    if (isNaN(a)) {
+        console.log('введенное значение не является числом');
+    } else {
+        console.log(`${a} в кубе равняется ${a ** 3}`);
+    }
+
+}
+exponent();
+
+// Задание 7
+function getArea() {
+    console.log(`Площадь окружности равна ${(this.radius * 2) ** 2 / 4 * Math.PI}`);
+}
+function getPerimeter() {
+    console.log(`Периметр окружности равен ${this.radius * 2 * Math.PI}`);
+}
+
+
+const circle1 = {
+    radius: 12,
+    Area: getArea,
+    Perimeter: getPerimeter
+}
+
+const circle2 = {
+    radius: 22,
+    Area: getArea,
+    Perimeter: getPerimeter
+}
+
+circle1.Area();
+circle1.Perimeter();
+circle2.Area();
+circle2.Perimeter();
